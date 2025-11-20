fd5fbe7c5c0fb308a3f1bd2a5a3d7e4355da7371
Jothammasila <133359184+Jothammasila@users.noreply.github.com>
2025-11-20 14:43:07 +0300

    Added simple regex tokenizer for splitting text into tokens

--- simple-tokenizer.py
+++ simple-tokenizer.py
@@ -1,1 +1,8 @@
+import re
+# Sample text
+# text = "Text is a book or other written/printed 'work', --regarded in terms of its content rather than its \"physical\" form."
 
+# Actual text data
+tokens = re.split(r'([\'\".,?_/()*]|--| -|\s)', text)
+tokens = [token.strip() for token in tokens if token.strip()]
+print(tokens)
